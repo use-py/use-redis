fa75be07171c63aabd9ea8695560b0c77c2c0ca6
miclon <jcnd@163.com>
2023-08-08 07:28:30 -0700

    🧪 test

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,3 +1,5 @@
+import threading
+
 import pytest
 
 from usepy_plugin_redis import useRedisStreamStore
@@ -8,8 +10,35 @@ def redis():
     return useRedisStreamStore()
 
 
+def test_connection(redis):
+    """
+    Test that the connection is established
+    """
+    assert redis.connection.ping()
+
+
 def test_send(redis):
     stream = 'test_stream'
     message = {'foo': 'bar'}
     redis.send(stream, message)
-    assert redis.connection.xlen(stream) == 1
+    assert redis.connection.xlen(stream) >= 1
+
+
+def test_consumer(redis):
+    stream = 'test_consumer_stream'
+    send_message = {'foo': 'bar'}
+    redis.send(stream, send_message)
+
+    def callback(message):
+        assert isinstance(message, list)
+        assert len(message) == 1
+        first_message = message[0]
+        message_id, message = first_message
+        _message = {
+            k.decode() if isinstance(k, bytes) else k: v.decode() if isinstance(v, bytes) else v
+            for k, v in message.items()
+        }
+        assert _message == send_message
+        redis.shutdown()
+
+    threading.Thread(target=redis.start_consuming, args=(stream, 'test_group', 'test_consumer', callback)).start()
