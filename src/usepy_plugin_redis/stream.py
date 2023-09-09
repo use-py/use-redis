7f15d61faf5353c84d3697dee9993fdfcf447ab0
NowAnti <believel.y@qq.com>
2023-09-09 15:39:56 +0800

    🦄 refactor: RedisStreamStore

--- src/usepy_plugin_redis/stream.py
+++ src/usepy_plugin_redis/stream.py
@@ -2,7 +2,7 @@ import json
 import logging
 import threading
 import time
-from typing import Optional, List
+from typing import Optional, List, Union, Callable
 
 import redis
 
@@ -16,55 +16,44 @@ class RedisStreamMessage:
     A message from the Redis stream.
     """
     
-    def __init__(self, message_id, message_body, stream_name, consumer_group, consumer_name: Optional[str] = None):
-        self.stream_name = stream_name
-        self.consumer_group = consumer_group
-        self.consumer_name = consumer_name
-        self.message_id = message_id
-        self.body = message_body
+    def __init__(self, id, body, stream, group):
+        self.stream = stream
+        self.group = group
+        self.id = id
+        self.body = body
     
     @staticmethod
-    def from_xread(raw_messages, *, consumer_group, consumer_name: Optional[str] = None):
+    def from_xread(raw_messages, *, group):
         """
         Parse a raw message from the Redis stream xread[group] respond.
         """
         # xread = [['test_consumer_stream', [('1693561850564-0', {'foo': 'bar'}), ('1693561905479-0', {'foo': 'bar'})]]]
         
-        if not consumer_group:
-            raise ValueError("consumer_group is required")
+        if not group:
+            raise ValueError("group is required")
         
         result = []
-        for stream_name, messages in raw_messages:
-            result.extend(
-                RedisStreamMessage(message_id, message_body, stream_name, consumer_group, consumer_name)
-                for message_id, message_body in messages
-            )
+        for stream, messages in raw_messages:
+            result.extend(RedisStreamMessage(id, body, stream, group) for id, body in messages)
         
         return result
     
     @staticmethod
-    def from_xclaim(raw_messages, *, stream_name, consumer_group, consumer_name: Optional[str] = None):
+    def from_xclaim(raw_messages, *, stream, group):
         """
         Parse a raw message from the Redis stream xclaim respond.
         """
         # xclaim = [('1693561850564-0', {'foo': 'bar'}), ('1693561905479-0', {'foo': 'bar'})]
         
-        if not stream_name:
-            raise ValueError("stream_name is required")
-        if not consumer_group:
-            raise ValueError("consumer_group is required")
+        if not stream:
+            raise ValueError("stream is required")
+        if not group:
+            raise ValueError("group is required")
         
-        return [RedisStreamMessage(message_id, message_body, stream_name, consumer_group, consumer_name)
-                for message_id, message_body in raw_messages]
+        return [RedisStreamMessage(id, body, stream, group) for id, body in raw_messages]
     
     def to_dict(self):
-        return {
-            "stream_name": self.stream_name,
-            "consumer_group": self.consumer_group,
-            "consumer_name": self.consumer_name,
-            "message_id": self.message_id,
-            "message_body": self.body
-        }
+        return {k: v for k, v in self.__dict__.items() if not k.startswith('_')}
     
     def to_json(self):
         return json.dumps(self.to_dict())
@@ -73,127 +62,125 @@ class RedisStreamMessage:
         return self.to_json()
     
     def __repr__(self):
-        return f"RedisStreamMessage({self.stream_name}, {self.consumer_group}, {self.message_id}, {self.body})"
+        return f"RedisStreamMessage({self.stream}, {self.group}, {self.id}, {self.body})"
 
 
 class RedisStreamStore(RedisStore):
     
-    def __init__(self, stream_name, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.stream_name = stream_name
+    def __init__(
+            self,
+            *,
+            stream: str,
+            group: str,
+            stream_max_entries: int = 0,
+            redeliver_timeout: int = 60000,
+            claim_interval: int = 1800000,
+            **kwargs
+    ):
+        """
+        
+        A Redis stream store.
+        
+        :param stream: The name of the stream.
+        :param group: The name of the group.
+        :param stream_max_entries: any value higher than 0 defines an approximate maximum number of stream entries
+        :param redeliver_timeout: Timeout before redeliver messages still in pending state (seconds)
+        :param claim_interval: Interval by which pending/abandoned messages should be checked
+
+        """
+        super().__init__(**kwargs)
+        self.stream = stream
+        self.group = group
+        self.max_entries = stream_max_entries if stream_max_entries > 0 else None
+        self.redeliver_timeout = redeliver_timeout
+        self.claim_interval = claim_interval
+        
+        self._auto_setup = True
         self.state = threading.local()
+    
+    def _setup(self):
+        try:
+            self.connection.xgroup_create(self.stream, self.group, id="0", mkstream=True)
+        except redis.exceptions.ResponseError as e:
+            if "already exists" not in str(e):
+                raise e
+        
+        self._auto_setup = False
     
     def send(self, message: dict):
         """
         Send a message to the Redis stream.
         """
-        try:
-            return self.connection.xadd(self.stream_name, message)
-        except redis.RedisError as e:
-            logger.error(f"Failed to send message: {e}")
-            raise
+        if self._auto_setup:
+            self._setup()
+        return self.connection.xadd(self.stream, message, maxlen=self.max_entries)
     
-    def _create_group(self, consumer_group, start_id="0-0"):
-        try:
-            self.connection.xgroup_create(self.stream_name, consumer_group, id=start_id, mkstream=True)
-        except redis.exceptions.ResponseError as e:
-            if "already exists" not in str(e):
-                raise e
-    
-    def _is_need_xclaim(self):
-        """判断是否需要 xclaim"""
-        return time.time() * 1000 - self.state.xclaim_last_time > self.state.xclaim_interval
-    
-    def _xautoclaim(
-            self,
-            consumer_name: str,
-            consumer_group: str,
+    def claim_old_pending_messages(
+            self, consumer: str,
             count: int,
             min_idle_time: int
     ) -> Optional[List[RedisStreamMessage]]:
         """
         Claim messages from the Redis stream.
         """
-        # TODO: 异常需要重试
-        try:
-            _, pending_messages, _ = self.connection.xautoclaim(
-                self.stream_name, consumer_group, consumer_name,
-                min_idle_time=min_idle_time,
-                start_id="0-0",
-                count=count
-            )
-            logger.debug(f"xautoclaim: {pending_messages=}")
-            if pending_messages:
-                return RedisStreamMessage.from_xclaim(
-                    raw_messages=pending_messages,
-                    stream_name=self.stream_name,
-                    consumer_group=consumer_group,
-                    consumer_name=consumer_name
-                )
-        except redis.RedisError as e:
-            logger.error(f"Error claiming messages: {e}")
-            time.sleep(self.RECONNECTION_DELAY)
+        # https://redis.io/commands/xautoclaim/
+        # 在迭代 PEL 时，如果偶然发现流中不再存在的消息（由 修剪 XDEL 或删除），它不会声明它 XAUTOCLAIM ，而是将其从找到它的 PEL 中删除。
+        # 此功能在 Redis 7.0 中引入。这些消息 ID 作为回复的一部分 XAUTOCLAIM 返回给调用方。
+        _, messages, _ = self.connection.xautoclaim(
+            name=self.stream,
+            groupname=self.group,
+            consumername=consumer,
+            min_idle_time=min_idle_time,
+            start_id="0",
+            count=count
+        )
+        logger.debug(f"xautoclaim: {messages=}")
+        
+        self.state.next_claim = time.time() * 1000 + self.claim_interval
+        
+        if messages:
+            return RedisStreamMessage.from_xclaim(raw_messages=messages, stream=self.stream, group=self.group)
     
-    def _xreadgroup(
-            self,
-            consumer_name: str,
-            consumer_group: str,
-            count: int,
-            block: Optional[int] = None
-    ) -> Optional[List[RedisStreamMessage]]:
+    def get(self, consumer: str, count: int = 1, block: Union[int, None] = None) -> Optional[List[RedisStreamMessage]]:
         """
-        Read messages from the Redis stream.
+        Read new messages from the Redis stream without claim.
         """
-        try:
-            raw_messages = self.connection.xreadgroup(
-                groupname=consumer_group,
-                consumername=consumer_name,
-                streams={self.stream_name: ">"},
-                count=count,
-                block=block,
-            )
-            logger.debug(f"xreadgroup: {raw_messages=}")
-            if raw_messages:
-                return RedisStreamMessage.from_xread(
-                    raw_messages=raw_messages,
-                    consumer_group=consumer_group,
-                    consumer_name=consumer_name,
-                )
-        except redis.RedisError as e:
-            logger.error(f"Error reading messages: {e}")
-            time.sleep(self.RECONNECTION_DELAY)
+        if self._auto_setup:
+            self._setup()
+        
+        raw_messages = self.connection.xreadgroup(
+            groupname=self.group,
+            consumername=consumer,
+            streams={self.stream: ">"},
+            count=count,
+            block=block
+        )
+        logger.debug(f"xreadgroup: {raw_messages=}")
+        if raw_messages:
+            return RedisStreamMessage.from_xread(raw_messages=raw_messages, group=self.group)
     
     def consume(
             self,
-            consumer_group: str,
-            consumer_name: str,
-            prefetch: int,
-            claim_min_idle_time: int = 3600000,
+            consumer: str,
+            prefetch: int = 1,
+            timeout: Union[int, None] = None,
             force_claim: bool = False,
-            block: Optional[int] = None
+            redeliver_timeout: Union[int, None] = None
     ) -> Optional[List[RedisStreamMessage]]:
+        
         """
         Consume messages from the Redis stream(order: xclaim -> xreadgroup).
         
-        :param consumer_name: 消费者名称
-        :param consumer_group: 消费组名称
-        :param prefetch: 消费数量
-        :param block: xreadgroup 阻塞时间，单位毫秒，默认为 None，不阻塞
-        :param claim_min_idle_time: xclaim 最小空闲的时间（即消息消费的超时时间），单位毫秒，默认为 1 小时
-        :param force_claim: 是否强制 xclaim，True=忽略上次 xclaim 的时间和 xclaim 间隔限制，False=按周期执行 xclaim
-        
-        :return: 消费的消息列表，消费失败时则返回 []
+        :param consumer: The name of the consumer
+        :param prefetch: Number of prefetches
+        :param timeout: Blocking time of the Xread. Unit is millisecond, 0 is infinite blocking
+        :param force_claim: Whether to force claim, True= to perform claim, False = execute claim periodically
+        :param redeliver_timeout: Timeout before redeliver messages still in pending state (seconds)
+        :return: List of messages consumed. If consumption fails, return []
         """
+        
         # 获取 当前消费者尚未ACK 的消息，最多获取  prefetch 个
-        pending_messages = self.connection.xpending_range(
-            name=self.stream_name,
-            groupname=consumer_group,
-            min='-',
-            max='+',
-            count=prefetch,
-            idle=0,
-            consumername=consumer_name,
-        )
+        pending_messages = self.connection.xpending_range(self.stream, self.group, '-', '+', prefetch, consumer, 0)
         # 计算需获取的消息数量
         need_count = prefetch - len(pending_messages) if pending_messages else prefetch
         if need_count <= 0:
@@ -201,46 +188,52 @@ class RedisStreamStore(RedisStore):
         
         result = []
         # 先尝试 xclaim
-        if force_claim or self._is_need_xclaim():
-            pending_messages = self._xautoclaim(consumer_name, consumer_group, need_count, claim_min_idle_time)
+        if force_claim or getattr(self.state, "next_claim", 0) <= time.time() * 1000:
+            pending_messages = self.claim_old_pending_messages(
+                consumer=consumer,
+                count=need_count,
+                min_idle_time=redeliver_timeout or self.redeliver_timeout
+            )
             if pending_messages:
                 result.extend(pending_messages)
-                # 更新还需获取的消息数量
-                need_count = need_count - len(result)
-            
-            self.state.xclaim_last_time = time.time() * 1000
+                need_count = need_count - len(result)  # 更新还需获取的消息数量
         
         # 然后 xreadgroup
         if need_count > 0:
-            messages = self._xreadgroup(consumer_name, consumer_group, need_count, block)
+            messages = self.get(consumer, need_count, timeout)
             if messages:
                 result.extend(messages)
         
         return result
     
-    def start_consuming(self, consumer_group, consumer_name, callback, prefetch=1, timeout=3600000, **kwargs):
+    def start_consuming(self, consumer: str, callback: Callable, prefetch: int = 1, timeout: int = 1000, **kwargs):
         """
         Start consuming messages from the Redis stream.
         
-        :param consumer_name: 消费者名称
-        :param consumer_group: 消费组名称
-        :param callback: 消费回调函数
-        :param prefetch: 消费并发数量
-        :param timeout: 消费超时时间(即 xclaim 最小空闲的时间)，默认为 1 小时
+        :param consumer: The name of the consumer，please use a unique value
+        :param callback: Callback function
+        :param prefetch: Number of prefetches
+        :param timeout: Blocking time of the Xread. Unit is millisecond, 0 is infinite blocking
         """
-        # 初始化工作
-        self._create_group(consumer_group, kwargs.get('group_start_id', '0-0'))
-        block = kwargs.get('xread_block', None)
-        
-        # xclaim 时必要的参数，得线程隔离
-        self.state.xclaim_interval = kwargs.get('xclaim_interval', 5 * 60 * 1000)  # xclaim 间隔时间，单位毫秒，默认 5 分钟
-        self.state.xclaim_last_time = 0
         
         while not self._shutdown:
             try:
-                messages = self.consume(consumer_group, consumer_name, prefetch, timeout, block=block)
+                messages = self.consume(consumer, prefetch, timeout=timeout, **kwargs)
                 for message in messages:
                     callback(message)
             except redis.RedisError as e:
                 logger.error(f"Error consuming messages: {e}")
                 time.sleep(self.RECONNECTION_DELAY)
+    
+    def ack(self, message: RedisStreamMessage):
+        """
+        Acknowledge a message.
+        """
+        self.connection.xack(self.stream, self.group, message.id)
+    
+    def reject(self, message: RedisStreamMessage):
+        """
+        Reject a message.
+        """
+        self.connection.xack(self.stream, self.group, message.id)
+        
--- tests/test_stream.py
+++ tests/test_stream.py
@@ -1,69 +1,92 @@
-import threading
+import json
 import time
 
+import redis
 import pytest
-
-from usepy_plugin_redis import useRedisStreamStore, RedisStreamMessage
-
-
-@pytest.fixture
-def redis():
-    return useRedisStreamStore(stream_name='test_stream')
+from usepy_plugin_redis.stream import RedisStreamStore, RedisStreamMessage
 
 
-def test_connection(redis):
-    """
-    Test that the connection is established
-    """
-    assert redis.connection.ping()
-
-
-def test_send(redis):
-    message = {'foo': 'bar'}
-    redis.send(message)
-    assert redis.connection.xlen('test_stream') >= 1
-
-
-def test_consumer(redis):
-    stream = 'test_consumer_stream'
-    redis.stream_name = stream
-    send_message = {'foo': 'bar'}
-    redis.send(send_message)
+# 测试RedisStreamMessage
+class TestRedisStreamMessage:
     
-    def callback(message: RedisStreamMessage, *args, **kwargs):
-        # print("message", message.to_dict())
-        assert message.body == send_message
-        redis.connection.xack(message.stream_name, message.consumer_group, message.message_id)
-        redis.shutdown()
+    def test_to_dict(self):
+        message = RedisStreamMessage('1', {'foo': 'bar'}, 'test_stream', 'test_group')
+        message_dict = message.to_dict()
+        assert isinstance(message_dict, dict)
+        assert message_dict['id'] == '1'
+        assert message_dict['body'] == {'foo': 'bar'}
     
-    # threading.Thread(target=redis.start_consuming, args=("consumer_group1", "consumer_name", callback,)).start()
-    threading.Thread(target=redis.start_consuming, args=("consumer_group2", "consumer_name", callback,),
-                     kwargs={'prefetch': 2}).start()
-    # threading.Thread(target=redis.start_consuming, args=("consumer_group3", "consumer_name", callback,)).start()
+    def test_to_json(self):
+        message = RedisStreamMessage('1', {'foo': 'bar'}, 'test_stream', 'test_group')
+        message_json = message.to_json()
+        assert isinstance(message_json, str)
+        assert json.loads(message_json) == {'id': '1', 'body': {'foo': 'bar'},
+                                            'stream': 'test_stream',
+                                            'group': 'test_group'}
 
 
-def test_consumer_xclaim(redis):
-    stream = 'test_consumer_xclaim_stream'
-    redis.connection.delete(stream)
+# 创建一个测试Redis连接
+def test_redis_connection():
+    connection = redis.StrictRedis(host='localhost', port=6379, db=0)
+    connection.flushall()  # 清空测试数据
+
+
+# 测试RedisStreamStore
+class TestRedisStreamStore:
     
-    redis.stream_name = stream
-    send_message = {'foo': 'bar'}
-    redis.send(send_message)
+    @pytest.fixture
+    def redis_stream_store(self):
+        store = RedisStreamStore(
+            stream='test_stream',
+            group='test_group'
+        )
+        yield store
     
-    def callback(message: RedisStreamMessage, *args, **kwargs):
-        # print("message", message.to_dict())
-        assert message.body == send_message
-        redis.connection.xack(message.stream_name, message.consumer_group, message.message_id)
-        redis.shutdown()
+    def test_claim_old_pending_messages(self, redis_stream_store):
+        consumer = 'test_consumer'
+        message = {'foo': 'bar'}
+        redis_stream_store.send(message)
+        redis_stream_store.get(consumer, 1)
+        messages = redis_stream_store.claim_old_pending_messages(consumer, count=1, min_idle_time=0)
+        assert isinstance(messages, list)
+        assert len(messages) == 1
+        assert isinstance(messages[0], RedisStreamMessage)
+        assert messages[0].body == message
+        redis_stream_store.ack(messages[0])
     
-    redis._create_group("consumer_group")
-    redis.consume("consumer_group", "consumer1", prefetch=1, claim_min_idle_time=0, force_claim=True)
-    time.sleep(1)
-    job = threading.Thread(target=redis.start_consuming,
-                           args=("consumer_group", "consumer2", callback,),
-                           kwargs={'prefetch': 1, 'xclaim_interval': 5000, 'timeout': 500, 'force_claim': True}
-                           )
-    job.start()
-    job.join()
+    def test_get_messages(self, redis_stream_store):
+        consumer = 'test_consumer'
+        message = {'foo': 'bar'}
+        redis_stream_store.send(message)
+        messages = redis_stream_store.get(consumer, count=1)
+        assert isinstance(messages, list)
+        assert len(messages) == 1
+        assert isinstance(messages[0], RedisStreamMessage)
+        assert messages[0].body == message
+        redis_stream_store.ack(messages[0])
     
+    def test_consume_messages(self, redis_stream_store):
+        consumer = 'test_consumer'
+        message = {'foo': 'bar'}
+        redis_stream_store.send(message)
+        messages = redis_stream_store.consume(consumer, prefetch=1, timeout=1000)
+        assert isinstance(messages, list)
+        assert len(messages) == 1
+        assert isinstance(messages[0], RedisStreamMessage)
+        assert messages[0].body == message
+        redis_stream_store.ack(messages[0])
     
+    def test_consume_messages_with_idle_time(self, redis_stream_store):
+        consumer = 'test_consumer'
+        message = {'foo': 'bar'}
+        redis_stream_store.send(message)
+        redis_stream_store.get(consumer + "_get", 1)
+        messages = redis_stream_store.consume(consumer, prefetch=1, force_claim=True, redeliver_timeout=500)
+        assert messages == []
+        time.sleep(1)
+        messages = redis_stream_store.consume(consumer, prefetch=1, force_claim=True, redeliver_timeout=500)
+        assert isinstance(messages, list)
+        assert len(messages) == 1
+        assert isinstance(messages[0], RedisStreamMessage)
+        assert messages[0].body == message
+        redis_stream_store.ack(messages[0])
